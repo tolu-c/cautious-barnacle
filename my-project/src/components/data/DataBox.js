1f08a07e63c3a0d1e09803c447ea90439bb38414
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-07-11 18:10:18 +0000

    fecthing data from graphql api✔

--- my-project/src/components/data/DataBox.js
+++ my-project/src/components/data/DataBox.js
@@ -1,8 +1,35 @@
 import DataGroup from "./DataGroup";
+import { gql, useQuery } from "@apollo/client";
+
+const statusQuery = gql`
+  {
+    status(id: 1) {
+      name
+      books {
+        id
+        name
+        created
+      }
+    }
+  }
+`;
 
 const DataBox = () => {
+  const { data, loading, error } = useQuery(statusQuery);
+
+  if (loading) return "Loading...";
+  if (error) return <pre>{error.message}</pre>;
+
+  console.log(data);
+
   return (
     <div className="flex flex-col items-start gap-y-8">
+      {data.status.books.map((book) => (
+        <div key={book.id}>
+          <p>{book.name}</p>
+          <span>Created: {book.created}</span>
+        </div>
+      ))}
       <DataGroup />
       <DataGroup />
       <DataGroup />
